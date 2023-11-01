fac3cf658dcab21ca249ce2b09c8427cb1a36a5f
Rhfialyndra <l>
2023-11-01 07:10:17 +0700

    add meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import DashboardLayout from '@/components/layout/DashboardLayout'
+import Head from 'next/head'
 
 export default function index() {
   return (
+  <>
+  <Head>
+    <title>Aster | Home</title>
+    <meta name="description" content={"Welcome to Aster's corner! This entire webapp is purposely made as an archive and to showcase some \"artworks\". The current version release only has animes, I will expand the feature and data later along the way."}/>
+    <meta lang="en-US"/>
+  </Head>
   <main className='w-full bg-emerald-500'>
 
-  </main>
+</main></>
   )
 }
 
